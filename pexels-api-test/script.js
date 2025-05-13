193da7c00fee90443e7153f6fac03d3d2b6b09ef
Neo <neogoldahlgren@gmail.com>
2025-05-13 11:30:44 +0200

    bla bla

--- pexels-api-test/script.js
+++ pexels-api-test/script.js
@@ -1,5 +1,5 @@
 async function getImages() {
-    const req = new Request("https://api.pexels.com/v1/search?query=Frogs", {
+    const req = new Request("https://api.pexels.com/v1/search?query=Frogs&per_page=30", {
         method: "GET",
         headers: {
             "Authorization": "sQLMQfpcJkVFD8dbejB6VqtaMkmnv7rIyaHrR45W2tOG5UWyaAeR4wfe"
@@ -10,9 +10,10 @@ async function getImages() {
     if (resp.ok) {
         let reso = await resp.json();
         console.log(reso);
-        let firstPhoto = reso.photos[5].src.small;
+        let num = Math.floor(Math.random() * reso.photos.length);
+        let photo = reso.photos[num].src.small;
         let img = document.createElement("img");
-        img.src = firstPhoto;
+        img.src = photo;
         document.querySelector("main").appendChild(img);
     } else {
         console.log(resp.status);
